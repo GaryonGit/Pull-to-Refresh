c7d07ff8b7331a9d3bc86ea691fcf1385489b69a
GaryonGit <gary@fromtheloft.com.au>
2014-02-28 20:55:08 +1000

    Gmail - Pull to Refresh
    A prototype built using framer.js to recreate the resistance of a Pull
    to Refresh and also to showcase something a bit more  fun than the
    Gmail iPhone refresh animation.

---
+++ app.js
@@ -0,0 +1,196 @@
+//============================================================
+// Settings
+//============================================================
+
+PSD.rocketIcon.y = -72;
+PSD.mainView.style.backgroundColor = "#606469";
+PSD.mailList.visible = false;
+
+
+// ===========================================================
+// Defining each animation frame of the Rocket as a function
+// ===========================================================
+
+rocket1 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: -1, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket2 = function(){PSD["rocketIcon"].animate({properties:{y:-74, x:4, rotationZ: 3, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket3 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: -3, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket4 = function(){PSD["rocketIcon"].animate({properties:{y:-70, x:2, rotationZ: 2, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket5 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 5, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket6 = function(){PSD["rocketIcon"].animate({properties:{y:-74, x:4, rotationZ: 0, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket7 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 2, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket8 = function(){PSD["rocketIcon"].animate({properties:{y:-70, x:0, rotationZ: 1, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket9 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 0, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket10 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: -1, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket11 = function(){PSD["rocketIcon"].animate({properties:{y:-74, x:4, rotationZ: 3, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket12 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: -3, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket13 = function(){PSD["rocketIcon"].animate({properties:{y:-70, x:2, rotationZ: 2, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket14 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 5, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket15 = function(){PSD["rocketIcon"].animate({properties:{y:-74, x:4, rotationZ: 0, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket16 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 2, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket17 = function(){PSD["rocketIcon"].animate({properties:{y:-70, x:0, rotationZ: 1, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket18 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 0, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket19 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: -1, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket20 = function(){PSD["rocketIcon"].animate({properties:{y:-74, x:4, rotationZ: 3, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket21 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: -3, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket22 = function(){PSD["rocketIcon"].animate({properties:{y:-70, x:2, rotationZ: 2, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket23 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 5, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket24 = function(){PSD["rocketIcon"].animate({properties:{y:-74, x:4, rotationZ: 0, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket25 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 2, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket26 = function(){PSD["rocketIcon"].animate({properties:{y:-70, x:0, rotationZ: 1, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket27 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 0, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket28 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: -1, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket29 = function(){PSD["rocketIcon"].animate({properties:{y:-74, x:4, rotationZ: 3, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket30 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: -3, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket31 = function(){PSD["rocketIcon"].animate({properties:{y:-70, x:2, rotationZ: 2, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket32 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 5, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket33 = function(){PSD["rocketIcon"].animate({properties:{y:-74, x:4, rotationZ: 0, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket34 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 2, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket35 = function(){PSD["rocketIcon"].animate({properties:{y:-70, x:0, rotationZ: 1, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket36 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: 0, scaleX: 1.1}, curve: "ease-in-out", time: 34})};
+rocket37 = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:2, rotationZ: -1, scaleX: 0.9}, curve: "ease-in-out", time: 34})};
+rocket38 = function(){PSD["rocketIcon"].animate({properties:{y:-74, x:4, rotationZ: 3, scaleX: 1}, curve: "ease-in-out", time: 34})};
+rocket39 = function(){PSD["rocketIcon"].animate({properties:{y:-60, x:2, rotationZ: 0, scaleX: 2.0, scaleY: 0.5}, curve: "ease-out", time: 150})};
+blastOff = function(){PSD["rocketIcon"].animate({properties:{y:-240, x:0, rotationZ: 0, scaleX: 0.5, scaleY: 1.5}, curve: "spring(700,80,2000)"})};  
+resetRocket = function(){PSD["rocketIcon"].animate({properties:{y:-72, x:0, rotationZ: 0, scaleX: 1, scaleY: 1}, curve: "ease-in-out", time: 34})};
+
+// =======================================================================================
+// Playing each of the functions above with offset delays so it behaves like a timeline
+// =======================================================================================
+rocketShake = function() { 
+  utils.delay(0, rocket1)
+  utils.delay(34, rocket2)
+  utils.delay(34*2, rocket3)
+  utils.delay(34*3, rocket4)
+  utils.delay(34*4, rocket5)
+  utils.delay(34*5, rocket6)
+  utils.delay(34*6, rocket7)
+  utils.delay(34*7, rocket8)
+  utils.delay(34*8, rocket9)
+  utils.delay(34*9, rocket10)
+  utils.delay(34*10, rocket11)
+  utils.delay(34*11, rocket12)
+  utils.delay(34*12, rocket13)
+  utils.delay(34*13, rocket14)
+  utils.delay(34*14, rocket15)
+  utils.delay(34*15, rocket16)
+  utils.delay(34*16, rocket17)
+  utils.delay(34*17, rocket18)
+  utils.delay(34*18, rocket19)
+  utils.delay(34*19, rocket20)
+  utils.delay(34*20, rocket21)
+  utils.delay(34*21, rocket22)
+  utils.delay(34*22, rocket23)
+  utils.delay(34*23, rocket24)
+  utils.delay(34*24, rocket25)
+  utils.delay(34*25, rocket26)
+  utils.delay(34*26, rocket27)
+  utils.delay(34*27, rocket28)
+  utils.delay(34*28, rocket29)
+  utils.delay(34*29, rocket30)
+  utils.delay(34*30, rocket31)
+  utils.delay(34*31, rocket32)
+  utils.delay(34*32, rocket33)
+  utils.delay(34*33, rocket34)
+  utils.delay(34*34, rocket35)
+  utils.delay(34*35, rocket36)
+  utils.delay(34*36, rocket37)
+  utils.delay(34*37, rocket38)
+  utils.delay(34*38, rocket39)
+  utils.delay(34*38+150, blastOff)
+  utils.delay(34*50, resetRocket)
+  
+};
+
+
+// Resistance (As per the one from http://codepen.io/koenbok/pen/AvGqn)
+
+(function() {
+  var boundRadius, boundsView, draggable, position, mailList;
+
+  position = {
+    x: 0,
+    y: 126
+  };
+
+// Determines how far I can pull the view creating the resistance bounds
+  boundRadius = 400;
+
+  mailList = new ImageView({
+    x: position.x,
+    y: position.y,
+    width: 640,
+    height: 1012,
+	image:"images/mailList.png",
+	superView:PSD.mainView
+      });
+
+  draggable = new ui.Draggable(mailList);
+
+  mailList.on(Events.TouchMove, function() {
+    var distance;
+    distance = {
+      y: Math.abs(mailList.y - position.y)
+    };
+    draggable.speed.x = 1 - Math.min(distance.x, boundRadius) / boundRadius;
+    return draggable.speed.y = 1 - Math.min(distance.y, boundRadius) / boundRadius;
+  });
+  
+//===============================================================================================
+//  Defining more functions 
+//===============================================================================================
+rubberBand = function() {return mailList.animate({properties: {y: 275}, curve: "spring(800,80,500)"})};
+afterBurner = function() {mailList.animate({properties: {y: 300} })};
+closeShop = function() {mailList.animate({properties: {y: 126}, curve: "spring(800,40,50)"})};
+
+  mailList.on(Events.TouchEnd, function() {
+      rubberBand();
+      rocketShake();
+      utils.delay(1434, afterBurner);
+      utils.delay(1472, closeShop); 
+  });
+
+  boundsView = new View({
+    width: mailList.width,
+    height: mailList.height
+  });
+
+  boundsView.midX = mailList.midX;
+  boundsView.midY = mailList.midY;
+  boundsView.placeBehind(mailList);
+
+}).call(this);
+
+
+
+// ============================================================
+// Alternative method to define the animation.
+// ============================================================
+
+// Rocket shaking
+// rocketShake = function() { 
+//   animation1 = new Animation({ view:PSD["rocketIcon"], properties:{y:-70, x:0, rotationZ: 3, scaleX: 1.1}, curve: "ease-in-out", time: 34});
+//   animation2 = new Animation({ view:PSD["rocketIcon"], properties:{y:-72, x:2, rotationZ: -1, scaleX: 0.9}, curve: "ease-in-out", time: 34});
+//   animation3 = new Animation({ view:PSD["rocketIcon"], properties:{y:-74, x:4, rotationZ: 3, scaleX: 1}, curve: "ease-in-out", time: 34});
+//   animation4 = new Animation({ view:PSD["rocketIcon"], properties:{y:-72, x:2, rotationZ: -3, scaleX: 1.1}, curve: "ease-in-out", time: 34});
+//   animation5 = new Animation({ view:PSD["rocketIcon"], properties:{y:-70, x:2, rotationZ: 2, scaleX: 0.9}, curve: "ease-in-out", time: 34});
+//   animation6 = new Animation({ view:PSD["rocketIcon"], properties:{y:-72, x:2, rotationZ: 5, scaleX: 1}, curve: "ease-in-out", time: 34});
+//   animation7 = new Animation({ view:PSD["rocketIcon"], properties:{y:-74, x:4, rotationZ: 0, scaleX: 1.1}, curve: "ease-in-out", time: 34});
+//   animation8 = new Animation({ view:PSD["rocketIcon"], properties:{y:-72, x:2, rotationZ: 2, scaleX: 1}, curve: "ease-in-out", time: 34});
+//   animation9 = new Animation({ view:PSD["rocketIcon"], properties:{y:-70, x:0, rotationZ: 1, scaleX: 0.9}, curve: "ease-in-out", time: 34});
+//   animation10 = new Animation({ view:PSD["rocketIcon"], properties:{y:-72, x:2, rotationZ: 0, scaleX: 2.1}, curve: "ease-in-out", time: 34});
+
+
+//   // Step throuh the animation
+//   animation1.on("end", animation2.start);
+//   animation2.on("end", animation3.start);
+//   animation3.on("end", animation4.start);
+//   animation4.on("end", animation5.start);
+//   animation5.on("end", animation6.start);
+//   animation6.on("end", animation7.start);
+//   animation7.on("end", animation8.start);
+//   animation8.on("end", animation9.start);
+//   animation9.on("end", animation10.start);
+//   animation10.on("end", animation1.start);
+//   animation1.start();
+// };
+
---
+++ framer/views.pullToRefresh.js
@@ -0,0 +1,152 @@
+		window.FramerPS = window.FramerPS || {};
+		window.FramerPS['pullToRefresh'] = [
+	{
+		"id": 56,
+		"name": "mainView",
+		"layerFrame": {
+			"x": 0,
+			"y": -40,
+			"width": 640,
+			"height": 1137
+		},
+		"maskFrame": {
+			"x": 0,
+			"y": -40,
+			"width": 640,
+			"height": 1136
+		},
+		"image": null,
+		"imageType": null,
+		"children": [
+			{
+				"id": 24,
+				"name": "Header",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 640,
+					"height": 1333
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/Header.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 640,
+						"height": 88
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "532558147"
+			},
+			{
+				"id": 58,
+				"name": "viewScroll",
+				"layerFrame": {
+					"x": 0,
+					"y": 88,
+					"width": 640,
+					"height": 1009
+				},
+				"maskFrame": {
+					"x": 0,
+					"y": 88,
+					"width": 640,
+					"height": 1008
+				},
+				"image": null,
+				"imageType": null,
+				"children": [
+					{
+						"id": 60,
+						"name": "view",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 640,
+							"height": 1333
+						},
+						"maskFrame": null,
+						"image": null,
+						"imageType": null,
+						"children": [
+							{
+								"id": 101,
+								"name": "rocketIcon",
+								"layerFrame": {
+									"x": 0,
+									"y": 0,
+									"width": 640,
+									"height": 1333
+								},
+								"maskFrame": null,
+								"image": {
+									"path": "images/rocketIcon.png",
+									"frame": {
+										"x": 301,
+										"y": 210,
+										"width": 39,
+										"height": 58
+									}
+								},
+								"imageType": "png",
+								"children": [
+									
+								],
+								"modification": "473601826"
+							},
+							{
+								"id": 133,
+								"name": "viewScrollContent",
+								"layerFrame": {
+									"x": 0,
+									"y": 0,
+									"width": 640,
+									"height": 1333
+								},
+								"maskFrame": null,
+								"image": null,
+								"imageType": null,
+								"children": [
+									{
+										"id": 135,
+										"name": "mailList",
+										"layerFrame": {
+											"x": 0,
+											"y": 0,
+											"width": 640,
+											"height": 1333
+										},
+										"maskFrame": null,
+										"image": {
+											"path": "images/mailList.png",
+											"frame": {
+												"x": 0,
+												"y": 88,
+												"width": 640,
+												"height": 1179
+											}
+										},
+										"imageType": "png",
+										"children": [
+											
+										],
+										"modification": "472262275"
+									}
+								],
+								"modification": "373641466"
+							}
+						],
+						"modification": "1269183685"
+					}
+				],
+				"modification": "131096836"
+			}
+		],
+		"modification": "465893451"
+	}
+]
